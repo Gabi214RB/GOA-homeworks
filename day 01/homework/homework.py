99f9d51863ebaf412672050ce78a6f4d7b7dc73a
Gabi214RB <gabrielqarosanidze@icloud.com>
2024-06-24 00:09:56 +0400

    first commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,12 @@
+name = "gabriel"
+surname ="karosanidze"
+age = ("13")
+wanttobe = ("a footballer")
+hobby = ("i have tt acaunt")
+friends = ("a lot of friends")
+cousin = ("brother")
+goodat = ("football")
+favorite_videogames = ("pubg brawl stars roblox")
+telefon_model = ("iphone XR")
+
+print(name + surname + str(age) +wanttobe + hobby + friends + cousin + goodat + favorite_videogames +  telefon_model)
